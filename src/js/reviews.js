e1da628265f2a8682a70be1ee770ba8b899370bf
fulaytar <fulaytar2405@gmail.com>
2024-03-14 02:07:16 +0200

    need load images  and styles

---
+++ src/js/reviews.js
@@ -0,0 +1,21 @@
+//Izitoast
+
+//import iziToast from "izitoast";
+//import "izitoast/dist/css/iziToast.min.css";
+
+//Бібліотека Axios
+//import axios from 'axios';
+
+//Swiper.js
+
+//import Swiper from 'swiper';
+// import Swiper styles
+//import 'swiper/css';
+//const swiper = new Swiper(...);
+
+//Accordion
+
+//import Accordion from 'accordion-js';
+//import 'accordion-js/dist/accordion.min.css';
+
+/* Видаляй зайве, якщо не використавуєш */
