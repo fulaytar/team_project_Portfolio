da807b0c1f090a2bf89ecc00f6d5dfae347e1250
Alina Alieks <alieksieienko.alina@gmail.com>
2024-03-15 18:42:26 +0200

    add effect parallax and fix width

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,10 +1,10 @@
 // Core version + navigation, keyboard, mousewheel modules:
 import Swiper from 'swiper';
-import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import { Navigation, Keyboard, Mousewheel, Parallax } from 'swiper/modules';
 
 // Import Swiper and modules styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/bundle';
 
 // Init Swiper:
 const swiper = new Swiper('.swiper', {
@@ -12,9 +12,10 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: false,
     speed: 500,
+    spaceBetween: 10,
 
     // Configure Swiper to use modules
-    modules: [Navigation, Keyboard, Mousewheel],
+    modules: [Navigation, Keyboard, Mousewheel, Parallax],
 
   // Navigation arrows
     navigation: {
@@ -29,5 +30,9 @@ const swiper = new Swiper('.swiper', {
     mousewheel: {
         enabled: true,
         forceToAxis: true,
-    }
+    },
+
+    // Parallax
+    parallax: true,
+
 });
