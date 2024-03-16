2a3a881fad14a004cc4fb10b3a4f8c51afb111fc
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-03-16 12:54:23 -0500

    in progress, css and adaptive css need to be done

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,21 +1,81 @@
-//Izitoast
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-//import iziToast from "izitoast";
-//import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
+import Swiper from 'swiper';
 
-//Бібліотека Axios
-//import axios from 'axios';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-//Swiper.js
 
-//import Swiper from 'swiper';
-// import Swiper styles
-//import 'swiper/css';
-//const swiper = new Swiper(...);
+const reviewsList = document.querySelector('.reviews');
 
-//Accordion
+function createReviewsMarkup(reviews) {
 
-//import Accordion from 'accordion-js';
-//import 'accordion-js/dist/accordion.min.css';
+const markup = reviews.map(({avatar_url, author, review}) =>
+    `<li class="review-item swiper-slide">
+      <img
+        class="review-image"
+        src="${avatar_url}"
+        alt="${author}"
+      />
+      <div class="review-textbox">
+      <h3 class="review-author">${author}</h3>
+      <p class="review-text">${review}</p>
+      </div>
+  </li>`)
+    .join("");
+  reviewsList.insertAdjacentHTML("beforeend", markup);
 
-/* Видаляй зайве, якщо не використавуєш */
+  const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 16,
+  // Responsive breakpoints
+  // breakpoints: {
+  //   375: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 16
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 16
+  //     }
+  //   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+}
+
+axios.defaults.baseURL = "https://portfolio-js.b.goit.study/api/";
+
+async function getReviews() {
+  const response = await axios.get("reviews");
+  return response.data;
+}
+ 
+try {
+    const reviews = await getReviews(); 
+    createReviewsMarkup(reviews);
+  } catch (error) {
+    iziToast.error({
+        fontSize: 'large',
+        position:	'topRight',
+        messageColor: 'white',
+        timeout:	5000,
+        backgroundColor: '#ED3B44',
+        theme: 'dark',
+        progressBar: false,
+        message: 'Reviews not found',
+    });
+    reviewsList.insertAdjacentHTML("beforeend",`<li class="review-item">
+    <p class="review-text">Not found</p></li>` );
+}
+  
+
