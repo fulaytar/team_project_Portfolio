52b3f4d8cf5094fd244c6309f156d728607cd86a
fulaytar <fulaytar2405@gmail.com>
2024-03-17 01:16:04 +0200

    done!

--- src/js/header.js
+++ src/js/header.js
@@ -20,44 +20,37 @@
 
 /* Видаляй зайве, якщо не використавуєш */
 
-//обробка бургера
 const header_close_btn = document.querySelector(".close");
 const header_burger_btn = document.querySelector(".burger");
+const mobileModal = document.querySelector('.mob-menu');
 
 header_close_btn.addEventListener("click", function() {
-        header_burger_btn.classList.toggle("activ-burger");
+    header_burger_btn.classList.toggle("activ-burger");
+    mobileModal.classList.toggle("is-open");
+    console.log(mobileModal);
+
+    // Додаємо відслідковувач події прокрутки після кліку на header_close_btn
+    window.addEventListener("scroll", scrollHandler);
 });
-    
-//обробка меню 
-const header_list = document.querySelector('.list-menu-header')
 
+function scrollHandler() {
+    // Перевіряємо, чи прокручено сторінку вниз
+    if (window.scrollY > 0) {
+        // Викликаємо функцію clickModalX() для закриття модального вікна
+        clickModalX();
+
+        // Видаляємо відслідковувач події прокрутки після виклику clickModalX()
+        window.removeEventListener("scroll", scrollHandler);
+    }
+}
+
+header_burger_btn.addEventListener("scroll", notScroll);
+
+// Обробка меню 
+const header_list = document.querySelector('.list-menu-header');
 const openMenuList = document.querySelector('.header_menu');
- 
 let status = false;
-
-openMenuList.addEventListener('click', openMenu);
-function openMenu() {
-    if (status) {
-        const items = document.querySelectorAll('.list-menu-header-item');
-        items.forEach((item, index) => {
-            if (index % 2 === 0) {
-                item.style.transform = 'translateX(-500%)';
-                item.style.opacity = '0';
-                item.style.visibility = 'hidden';
-                item.style.pointerEvents = 'none';
-            } else {
-                item.style.transform = 'translateX(500%)';
-                item.style.opacity = '0';
-                item.style.visibility = 'hidden';
-                item.style.pointerEvents = 'none';
-            }
-        });
-        header_list.style.opacity = '0';
-        header_list.style.visibility = 'hidden';
-        header_list.style.pointerEvents = 'none';
-        status = false;
-    } else {
-        const cssText = `
+const cssText = `
             display: flex;
             text-align: center;
             flex-direction: column;
@@ -70,17 +63,82 @@ function openMenu() {
             visibility: visible;
             pointer-events: auto;
         `;
-        header_list.style.cssText = cssText;
 
-        const items = document.querySelectorAll('.list-menu-header-item');
-        items.forEach((item, index) => {
-            item.style.transform = 'translateX(0)';
-            item.style.opacity = '1';
-            item.style.visibility = 'visible';
-            item.style.pointerEvents = 'auto';
-            
-        });
+openMenuList.addEventListener('click', menuHandler);
 
-        status = true;
+function menuHandler() {
+    if (status) {
+         closeMenu();
+        return;
+    } 
+    openMenu();
+    window.addEventListener('scroll', checkScroll);
+}
+
+function closeMenu() {
+    const items = document.querySelectorAll('.list-menu-header-item');
+    items.forEach((item, index) => {
+        if (index % 2 === 0) {
+            item.style.transform = 'translateX(-500%)';
+            item.style.opacity = '0';
+            item.style.visibility = 'hidden';
+            item.style.pointerEvents = 'none';
+        } else {
+            item.style.transform = 'translateX(500%)';
+            item.style.opacity = '0';
+            item.style.visibility = 'hidden';
+            item.style.pointerEvents = 'none';
+        }
+    });
+    header_list.style.opacity = '0';
+    header_list.style.visibility = 'hidden';
+    header_list.style.pointerEvents = 'none';
+    status = false;
+}
+
+function openMenu() {
+    header_list.style.cssText = cssText;
+
+    const items = document.querySelectorAll('.list-menu-header-item');
+    items.forEach((item) => {
+        item.style.transform = 'translateX(0)';
+        item.style.opacity = '1';
+        item.style.visibility = 'visible';
+        item.style.pointerEvents = 'auto';
+    });
+    status = true;
+}
+
+function checkScroll() {
+    // Отримати кількість пікселів, на які прокручено сторінку по вертикалі
+    const scrolledPixels = window.scrollY;
+    console.log('Прокручено на ' + scrolledPixels + ' пікселів.');
+
+    // Тепер ви можете використати це значення для вашої логіки
+    // Наприклад, ви можете перевірити, чи сторінка прокручена на певну кількість пікселів і виконати відповідні дії
+     if (scrolledPixels > 160 && status) {
+         closeMenu();
+         window.removeEventListener("scroll", checkScroll);
+    } 
+}
+
+const header_close_btn_modal = document.querySelector(".close-modal");
+const header_burger_btn_modal = document.querySelector(".burger-modal");
+
+header_close_btn_modal.addEventListener("click", clickModalX);
+header_close_btn_modal.addEventListener('scroll', notScroll);
+
+function clickModalX() {
+    header_burger_btn_modal.classList.toggle("activ-burger");
+    header_burger_btn.classList.toggle("activ-burger");
+    mobileModal.classList.toggle("is-open");
+}
+
+function notScroll() {
+    let scrolledPixels = window.scrollY;
+    console.log(scrolledPixels);
+    if (scrolledPixels > 5) {
+        clickModalX();
+        window.removeEventListener("scroll", notScroll);
     }
 }
