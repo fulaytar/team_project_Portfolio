0edf29ba656baf5519c2332fa96cf6cb76de90fb
dimentiygg <sfx670@gmail.com>
2024-03-17 22:23:19 +0200

    done!!!

--- src/js/about_me.js
+++ src/js/about_me.js
@@ -1,18 +1,17 @@
-'use strict'
+'use strict';
 import Swiper from 'swiper';
 import 'swiper/css';
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
 
 const arrow = document.querySelectorAll('.arrow');
-const ac = document.querySelector('.accordion-container');
-const container = document.querySelector('.accordion-container');
+const container = document.querySelector('.accordion-wrapper');
 
-const accordion = new Accordion(container, {
+const aboutMeAccordion = new Accordion(container, {
   showMultiple: true,
 });
 
-ac.addEventListener('click', event => {
+container.addEventListener('click', event => {
   var button = event.target.closest('button');
   if (button && button.tagName === 'BUTTON') {
     if (button.id === 'ac-trigger-0') {
@@ -31,9 +30,6 @@ const swiper = new Swiper('.swiper2', {
   loop: true,
   slidesPerView: 2,
   spaceBetween: 0,
-  navigation: {
-    nextEl: '.arrow',
-  },
 });
 
 nextArrow.addEventListener('click', () => {
