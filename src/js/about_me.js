627ea7ba979b81e9d1c3d74f0bd945dac629173d
fulaytar <144909744+fulaytar@users.noreply.github.com>
2024-03-17 19:23:49 +0200

    About me section (#20)
    * about-me
    
    * Update about_me.html
    
    * done
    
    * refactoring
    
    ---------
    
    Co-authored-by: dimentiygg <sfx670@gmail.com>

--- src/js/about_me.js
+++ src/js/about_me.js
@@ -1,51 +1,22 @@
-'use strict'
-import Swiper from 'swiper';
-import 'swiper/css';
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+//Izitoast
 
-const arrow = document.querySelectorAll('.arrow');
-const ac = document.querySelector('.accordion-container');
-const container = document.querySelector('.accordion-container');
+//import iziToast from "izitoast";
+//import "izitoast/dist/css/iziToast.min.css";
 
-const accordion = new Accordion(container, {
-  showMultiple: true,
-});
+//Бібліотека Axios
 
-ac.addEventListener('click', event => {
-  var button = event.target.closest('button');
-  if (button && button.tagName === 'BUTTON') {
-    if (button.id === 'ac-trigger-0') {
-      arrow[0].classList.toggle('rotate');
-    } else if (button.id === 'ac-trigger-1') {
-      arrow[1].classList.toggle('rotate');
-    } else if (button.id === 'ac-trigger-2') {
-      arrow[2].classList.toggle('rotate');
-    }
-  }
-});
+//import axios from 'axios';
 
-const nextArrow = document.querySelector('.arrow-next');
+//Swiper.js
 
-const swiper = new Swiper('.swiper2', {
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 0,
-  navigation: {
-    nextEl: '.arrow',
-  },
-});
+//import Swiper from 'swiper';
+// import Swiper styles
+//import 'swiper/css';
+//const swiper = new Swiper(...);
 
-nextArrow.addEventListener('click', () => {
-  swiper.slideNext();
-});
+//Accordion
 
-if (window.innerWidth > 767 && window.innerWidth < 1440) {
-  swiper.params.slidesPerView = 3;
-  swiper.update();
-}
+//import Accordion from 'accordion-js';
+//import 'accordion-js/dist/accordion.min.css';
 
-if (window.innerWidth >= 1440) {
-  swiper.params.slidesPerView = 6;
-  swiper.update();
-}
+/* Видаляй зайве, якщо не використавуєш */
