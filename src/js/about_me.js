ec76c5285a6d2ba6d4f9678fd5375b1949afea54
fulaytar <fulaytar2405@gmail.com>
2024-03-19 15:16:26 +0200

    Revert "remade accordion to ul + html and css fix (#45)"
    This reverts commit 6583e4a7149043eda04e5245cf3497d249097e81.

--- src/js/about_me.js
+++ src/js/about_me.js
@@ -6,7 +6,7 @@ import Accordion from 'accordion-js';
 // import 'accordion-js/dist/accordion.min.css';
 
 const arrow = document.querySelectorAll('.arrow');
-const container = document.querySelector('.accordion-menu');
+const container = document.querySelector('.accordion-wrapper');
 
 const aboutMeAccordion = new Accordion(container, {
   showMultiple: true,
@@ -14,7 +14,6 @@ const aboutMeAccordion = new Accordion(container, {
 aboutMeAccordion.open(0);
 
 container.addEventListener('click', event => {
-
   const button = event.target.closest('button');
   if (button && button.tagName === 'BUTTON') {
     if (button.id === 'ac-trigger-0') {
