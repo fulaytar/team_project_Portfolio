bc9055b91f2cd21409119c1259ce6a4147344088
Alina Alieks <alieksieienko.alina@gmail.com>
2024-03-19 17:38:08 +0200

    clean code

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 import Swiper from 'swiper';
 
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
-// import 'swiper/css/bundle';
 
 
 const reviewsList = document.querySelector('.reviews');
