b8c3747dd7de13985ef2e32244a4a21f60948d48
LemonJackson <107397445+LemonJackson@users.noreply.github.com>
2024-03-21 21:52:17 +0200

    Update About me (#53)
    Co-authored-by: fulaytar <144909744+fulaytar@users.noreply.github.com>

--- src/js/about_me.js
+++ src/js/about_me.js
@@ -1,38 +1,20 @@
-'use strict';
-import Swiper from 'swiper';
-import 'swiper/css';
-import { Navigation } from 'swiper/modules';
 import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
 
-const arrow = document.querySelectorAll('.arrow');
-const container = document.querySelector('.accordion-wrapper');
-
-const aboutMeAccordion = new Accordion(container, {
+const container = document.querySelector('.accordion-container-about');
+const accordion = new Accordion(container, {
   showMultiple: true,
-});
-aboutMeAccordion.open(0);
-
-container.addEventListener('click', event => {
-  const button = event.target.closest('button');
-  if (button && button.tagName === 'BUTTON') {
-    if (button.id === 'ac-trigger-0') {
-      arrow[0].classList.toggle('rotate');
-    } else if (button.id === 'ac-trigger-1') {
-      arrow[1].classList.toggle('rotate');
-    } else if (button.id === 'ac-trigger-2') {
-      arrow[2].classList.toggle('rotate');
-    }
-  }
+  openOnInit: [0],
 });
 
-const nextArrow = document.querySelector('.arrow-next');
 
 const AboutMeSwiper = new Swiper('.swiper2', {
   loop: true,
   slidesPerView: 2,
   spaceBetween: 0,
-  modules: [Navigation],
+  modules: [Navigation, Keyboard, Mousewheel],
 
   navigation: {
     nextEl: '.sbn',
@@ -46,5 +28,12 @@ const AboutMeSwiper = new Swiper('.swiper2', {
       slidesPerView: 6,
     },
   },
-});
 
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    enabled: true,
+    forceToAxis: true,
+  },
+});
